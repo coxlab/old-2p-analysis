f275909e23c6e05984e52eb4d3a0d033f94dcd72
Ben Vermaercke <ben.vermaercke@gmail.com>
2015-04-20 17:26:52 -0400

    Added option to prepend medfilt3
    Apply this before calculating z-projection to improve quality of the
    MIPs

--- step02b_calc_MIPs.m
+++ step02b_calc_MIPs.m
@@ -5,10 +5,16 @@ clc
 % - use frame to frame shift to detect homogeneous blocks (=noisy)
 % - average over blocks and detect big shifts (=reliable)
 % - compute offset between block average and single frames within block (=better reliability then step 01) 
+%
+% BV20150420: prepend median filter 1-1-3 to MIP calculations, won't affect
+% raw file, so don't care about temporal smoothing this causes.
 
 header_script
 
 debugging=0;
+MIP_options.do_medfilt3=1;
+MIP_options.medfilt_values=[1 1 3]; % default 1-1-3, could try 1-1-5
+MIP_options.apply_motion_correction=1; % try only if shift_values are present
 
 %%% Use uigetdir to get data folder
 cd(data_root)
@@ -33,16 +39,16 @@ for iSess=1:nSessions
     frames=zeros(rows-1,cols,nFrames);
     if isfield(session_data,'motion_correction')
         motion_correction=session_data.motion_correction;
-        apply_motion_correction=1;
+        MIP_options.apply_motion_correction=1;
     else
-        apply_motion_correction=0;
+        MIP_options.apply_motion_correction=0;
     end
     tic
     for iFrame=1:nFrames
         frame=double(imread(session_data.file_name,iFrame,'info',info));
         frame=double(frame(1:end-1,:)); % no flyback line
         
-        if apply_motion_correction==1
+        if MIP_options.apply_motion_correction==1
             %%% BV20150409: use motion correction here to offset frames
             % will create boundary issues
             offset=-motion_correction.shift_matrix(iFrame,4:5);
@@ -57,16 +63,22 @@ for iSess=1:nSessions
         frames(:,:,iFrame)=frame;
     end
     fprintf('Loading frames took %3.2f seconds.\n',toc)
+    if MIP_options.do_medfilt3==1
+        %% BV20150420: added 3D median filt, will make images look prettier, especially max
+        tic
+        frames=medfilt3(frames,MIP_options.medfilt_values);
+        fprintf('Medfilt3(A,[%d %d %d]) took %3.2f seconds.\n',[MIP_options.medfilt_values toc])
+    end    
     
     %% calc projection images
     tic
     disp('Computing MIPs...')
     session_data.MIP_avg.data=mean(frames,3);
-    session_data.MIP_avg.gamma_val=1;
+    session_data.MIP_avg.gamma_val=.6;
     session_data.MIP_max.data=max(frames,[],3);
     session_data.MIP_max.gamma_val=1;
     session_data.MIP_std.data=std(frames,[],3);
-    session_data.MIP_std.gamma_val=1;
+    session_data.MIP_std.gamma_val=.3;
     if debugging==0 % only if we are not debugging, takes a long time to run
         session_data.MIP_cc_local.data=CrossCorrImage(frames);
         session_data.MIP_cc_local.gamma_val=1;
@@ -91,12 +103,13 @@ for iSess=1:nSessions
         imshow(calc_gamma(session_data.MIP_std.data,session_data.MIP_std.gamma_val),[])
         %subplot(224)
         %imshow(calc_gamma(session_data.MIP_cc_local.data,session_data.MIP_cc_local.gamma_val),[])
-        %colormap(green)
+        colormap(green)
     end
     %session_data.frames=frames;
     
     if debugging==0
         %%
+        session_data.MIP_options=MIP_options;
         [save_folder, save_name]=fileparts(session_data.file_name);
         saveName=fullfile(save_folder,'data_analysis',[save_name '.mat'])
         save(saveName,'session_data')
