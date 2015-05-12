cb6a7da63cc22f02f2b3767dd927b092bc83362c
Ben Vermaercke <ben.vermaercke@gmail.com>
2015-05-12 13:19:45 -0400

    Folder selection

--- header_script.m
+++ header_script.m
@@ -42,7 +42,8 @@ switch 0
         %exp_name='20150502_jrat3/Session02';
         %exp_name='20150502_jrat3/Session03-05';
         %exp_name='20150502_jrat3/Session06-10';
-        exp_name='20150502_jrat3/Session11-16';
+        %exp_name='20150502_jrat3/Session11-16';
+        exp_name='20150502_jrat3/Session17-22';
         
         %data_folder=['/Users/' user_name '/Dropbox (coxlab)/2p-data/' exp_name];
         data_folder=fullfile(data_root,exp_name);
