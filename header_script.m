a4039503fdf28660981f8dbb8cb5b65867428696
Ben Vermaercke <ben.vermaercke@gmail.com>
2015-05-11 23:37:27 -0400

    Folder selection

--- header_script.m
+++ header_script.m
@@ -28,9 +28,14 @@ green=[zeros(256,1) linspace(0,1,256)' zeros(256,1)];
 switch 0
     case 0 % hardcoded
         %exp_name='2015-03-03_AF03-light_awake';
+        %exp_name='2015-03-04_AF11';
         %exp_name='2015-03-05_AF03';
-        exp_name='2015-03-04_AF11';
+        exp_name='2015-03-05_AF11';
         %exp_name='2015-04-07_AF11';
+        %exp_name='2015-04-10_AF11_exp';
+        %exp_name='2015-04-15_AF11';
+        %exp_name='2015-04-16_AF11';
+        
         
         %data_folder=['/Users/' user_name '/Dropbox (coxlab)/2p-data/' exp_name];
         data_folder=fullfile(data_root,exp_name);
