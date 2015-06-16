2e5d6aeffa046f249ec52a0280b51d523ab42567
Ben Vermaercke <ben.vermaercke@gmail.com>
2015-06-16 09:28:50 -0400

    Created new function to get all events with specified tag name

---
+++ includes/MWorks/get_events_by_name.m
@@ -0,0 +1,27 @@
+function varargout=get_events_by_name(varargin)
+%[events,nEvents,event_code]=get_events_by_name(mwk_file_name,tag_find [,event_codec])
+
+%%% Parse inputs
+mwk_file_name=varargin{1};
+tag_find=varargin{2};
+
+
+if nargin>=3&&~isempty(varargin{3})
+    event_codec=varargin{3};
+else
+    A=getCodecs(mwk_file_name);
+    event_codec=A.codec;
+end
+
+%%% build tag list and find code needed for selection
+tag_list={event_codec.tagname}';
+code_list=cat(1,event_codec.code);
+code_selection=code_list(ismember(tag_list,tag_find));
+
+%%% Read events with given event_code from file
+events=getEvents(mwk_file_name, code_selection);
+
+%%% Construct outputs
+varargout{1}=events;
+varargout{2}=length(events);
+varargout{3}=code_selection;
