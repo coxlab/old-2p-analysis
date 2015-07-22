53e1b34ea8557cc8048a8b7deffb72e532e51a6e
Ben Vermaercke <ben.vermaercke@gmail.com>
2015-07-21 23:14:21 -0400

    Allowed timestamp range to be specified

--- includes/MWorks/get_events_by_name.m
+++ includes/MWorks/get_events_by_name.m
@@ -5,12 +5,23 @@ function varargout=get_events_by_name(varargin)
 mwk_file_name=varargin{1};
 tag_find=varargin{2};
 
-
 if nargin>=3&&~isempty(varargin{3})
     event_codec=varargin{3};
 else
     A=getCodecs(mwk_file_name);
     event_codec=A.codec;
+end
+
+if nargin>=4&&~isempty(varargin{4})
+    start_time=varargin{4};
+else
+    start_time=[];
+end
+
+if nargin>=5&&~isempty(varargin{5})
+    end_time=varargin{5};
+else
+    end_time=[];
 end
 
 %%% build tag list and find code needed for selection
@@ -20,7 +31,11 @@ found=ismember(tag_list,tag_find);
 if any(found)
     code_selection=code_list(found);
     %%% Read events with given event_code from file
-    events=getEvents(mwk_file_name, code_selection);
+    if ~isempty(start_time)&&~isempty(end_time)
+        events=getEvents(mwk_file_name, code_selection, start_time, end_time);
+    else
+        events=getEvents(mwk_file_name, code_selection);
+    end
     
     %%% Construct outputs
     varargout{1}=events;
