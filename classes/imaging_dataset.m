ecf3a651959510d47beafb11e7ff41f841e7db1c
Ben Vermaercke <ben.vermaercke@gmail.com>
2015-07-26 23:20:55 -0400

    Created new class for combined data_sessions

---
+++ classes/imaging_dataset.m
@@ -0,0 +1,12 @@
+classdef imaging_dataset < handle
+    properties
+        
+    end
+    
+    methods
+        function self=imaging_dataset(varargin)
+            varargin
+            
+        end
+    end
+end
