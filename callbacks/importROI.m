23854cc286580cd30b74a40c03fc02bd3feea1d5
Ben Vermaercke <ben.vermaercke@gmail.com>
2015-07-31 15:32:04 -0400

    Allow to import automatically extracted ROIs

--- callbacks/importROI.m
+++ callbacks/importROI.m
@@ -1,62 +1,81 @@
 function importROI(varargin)
 H=varargin{1};
-handles=guidata(H);
-
-%%% Get file to load ROIs from
-cd(fullfile(handles.data_folder,'data_analysis'))
-[filename,pathname]=uigetfile('.mat');
-loadName=fullfile(pathname,filename);
-
-%%% Load data from file
-load(loadName,'session_data')
-
-
-%%% Load ROI definitions from selected session_data
-ROI=get_ROI_definitions(session_data,handles.ROI_definition_nr);
-nROI=length(ROI);
-
-%%% implement shift to coords, based on offset between MIP images
-im1=handles.MIP; % MIP from current file
-if isfield(session_data,'MIP_std')
-    if isfield(session_data.MIP_std,'data')
-        im2=session_data.MIP_std.data; % temp MIP from other, existing file
-    else
-        im2=session_data.MIP_std; % for older version old
-    end
-elseif isprop(session_data,'MIP_std')
-    im2=session_data.MIP_std.data; % temp MIP from other, existing file
+if nargin>=3&&~isempty(varargin{3})
+    source=varargin{3};
 else
-    session_data
-    die
+    source=1;
 end
 
-%%% Check GUI checkbox whether to align automatically or not
-if get(handles.auto_align,'value')==1
-    if numel(im1)>numel(im2)
-        A=im1;
-        template=im2;
-        ref=1;
-    else
-        A=im2;
-        template=im1;
-        ref=2;
-    end
-    CC=normxcorr2(template,A);
-    
-    % get shift coordinates relative to biggest image
-    CC_max=max(CC(:));
-    [i,j]=find(CC==CC_max);
-    
-    peakX=j-size(template,2)/2+1;
-    peakY=i-size(template,1)/2+1;
-    
-    if ref==1 % make shift relative to first image
-        offset=-[peakX-size(template,2)/2 peakY-size(template,1)/2]-[1 0];
-    else
-        offset=[peakX-size(template,2)/2 peakY-size(template,1)/2]-[1 0];
-    end
-    
-    % move all coordinates with the offset found    
+handles=guidata(H);
+
+switch source
+    case 1 % another session
+        %%% Get file to load ROIs from
+        cd(fullfile(handles.data_folder,'data_analysis'))
+        [filename,pathname]=uigetfile('.mat');
+        loadName=fullfile(pathname,filename);
+        
+        %%% Load data from file
+        load(loadName,'session_data')
+        
+        %%% Load ROI definitions from selected session_data
+        ROI=get_ROI_definitions(session_data,handles.ROI_definition_nr);
+        nROI=length(ROI);
+        
+        %%% implement shift to coords, based on offset between MIP images
+        im1=handles.MIP; % MIP from current file
+        if isfield(session_data,'MIP_std')
+            if isfield(session_data.MIP_std,'data')
+                im2=session_data.MIP_std.data; % temp MIP from other, existing file
+            else
+                im2=session_data.MIP_std; % for older version old
+            end
+        elseif isprop(session_data,'MIP_std')
+            im2=session_data.MIP_std.data; % temp MIP from other, existing file
+        else
+            session_data
+            die
+        end
+        
+        %%% Check GUI checkbox whether to align automatically or not
+        if get(handles.auto_align,'value')==1
+            if numel(im1)>numel(im2)
+                A=im1;
+                template=im2;
+                ref=1;
+            else
+                A=im2;
+                template=im1;
+                ref=2;
+            end
+            CC=normxcorr2(template,A);
+            
+            % get shift coordinates relative to biggest image
+            CC_max=max(CC(:));
+            [i,j]=find(CC==CC_max);
+            
+            peakX=j-size(template,2)/2+1;
+            peakY=i-size(template,1)/2+1;
+            
+            if ref==1 % make shift relative to first image
+                offset=-[peakX-size(template,2)/2 peakY-size(template,1)/2]-[1 0];
+            else
+                offset=[peakX-size(template,2)/2 peakY-size(template,1)/2]-[1 0];
+            end
+        end
+        
+    case 2 % auto ROIs
+        ROI=get_ROI_definitions(handles.session_data,1);
+        nROI=length(ROI);
+        
+        offset=[0 0];
+        loadName=handles.session_data.save_name;
+end
+
+% move all coordinates with the offset found
+if nROI==0
+    disp('No ROIs found')
+else
     for iROI=1:nROI
         ROI(iROI).base_coord=ROI(iROI).base_coord-offset;
         ROI(iROI).coords=ROI(iROI).coords-repmat(offset,size(ROI(iROI).coords,1),1);
@@ -68,7 +87,7 @@ if get(handles.auto_align,'value')==1
         ROI(iROI).ROI_rect=ROI(iROI).ROI_rect-repmat(offset,size(ROI(iROI).ROI_rect,1),2);
     end
     fprintf('Shifted all coordinates by x=%d and y=%d \n',offset)
-        
+    
     %%% Check if coords are still valid after the shift
     if isfield(handles.session_data,'data')
         Height=handles.session_data.data(4);
@@ -93,18 +112,18 @@ if get(handles.auto_align,'value')==1
     if nRemoved>0
         fprintf('Removed %d ROI that became invalid after shift \n',nRemoved)
     end
+    
+    
+    %%% Import ROIs from other file
+    handles.ROI=ROI;
+    handles.ROI_selector=1;
+    handles.nROI=length(handles.ROI);
+    handles.ROI_temp=handles.ROI_empty;
+    
+    set(handles.global_properties_table,'value',handles.ROI_selector)
+    fprintf('Imported %d out of %d ROIs from %s \n',[handles.nROI nROI loadName])
+    
+    guidata(H,handles)
+    
+    update_GUI(H)
 end
-
-%%% Import ROIs from other file
-handles.ROI=ROI;
-handles.ROI_selector=1;
-handles.nROI=length(handles.ROI);
-handles.ROI_temp=handles.ROI_empty;
-
-set(handles.global_properties_table,'value',handles.ROI_selector)
-fprintf('Imported %d out of %d ROIs from %s \n',[handles.nROI nROI loadName])
-
-guidata(H,handles)
-
-update_GUI(H)
-
