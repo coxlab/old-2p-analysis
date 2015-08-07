d8012d1f80cecb54d693bac70af4ceec4bbd945c
Ben Vermaercke <ben.vermaercke@gmail.com>
2015-08-07 11:29:44 -0400

    Make path to file machine independent

--- tools/test_GPU.m
+++ tools/test_GPU.m
@@ -1,7 +1,8 @@
 clear all
 clc
 
-load('/Users/benvermaercke/Dropbox (coxlab)/2p-data/2015-08-06_AH02/data_analysis/2015-08-06_AH02_001.mat')
+header_script
+load(fullfile(data_folder,'2015-08-06_AH02_001.mat'))
 
 %%
 [CC_max,offset]=im_align(session_data.MIP_avg.data,session_data.MIP_cc_local.data);
