07b068e5a955d88c30de72d8f08bbe9f9f7a4929
Ben Vermaercke <ben.vermaercke@gmail.com>
2015-08-07 11:30:31 -0400

    Wrong path

--- tools/test_GPU.m
+++ tools/test_GPU.m
@@ -2,7 +2,7 @@ clear all
 clc
 
 header_script
-load(fullfile(data_folder,'2015-08-06_AH02_001.mat'))
+load(fullfile(data_folder,'data_analysis','2015-08-06_AH02_001.mat'))
 
 %%
 [CC_max,offset]=im_align(session_data.MIP_avg.data,session_data.MIP_cc_local.data);
