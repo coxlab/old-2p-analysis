c5e14aab1765694002d08ff37adc6ae35d3183a6
Ben Vermaercke <ben.vermaercke@gmail.com>
2015-08-08 09:02:41 -0400

    Folder selection

--- header_script.m
+++ header_script.m
@@ -51,7 +51,8 @@ switch 0
         %% 1024x300 sessions
         %exp_name='2015-08-03_AH02_init'; % no scim bitcodes...
         %exp_name='2015-08-03_AH02_init/resaved'; % recovered scim bitcodes!
-        exp_name='2015-08-06_AH02';
+        %exp_name='2015-08-06_AH02';
+        exp_name='2015-08-07_AH03';
         
         %exp_name='2015-03-05_AF11/resaved'; % recovered scim bitcodes!
         
