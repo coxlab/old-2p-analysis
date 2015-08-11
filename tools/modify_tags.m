0e67ea48b82d88919041548e2004e0b513392e4b
Ben Vermaercke <ben.vermaercke@gmail.com>
2015-08-11 11:11:45 -0400

    Added new case for 2015-08-10_AH02

--- tools/modify_tags.m
+++ tools/modify_tags.m
@@ -7,7 +7,7 @@ else
     root_folder='D:\Dropbox (coxlab)\2p-data\';
 end
 
-switch 1
+switch 3
     case 1 % 2015-08-03_AH02_init
         data_folder=fullfile(root_folder,'2015-08-03_AH02_init/resaved');
         src_file_name='2015-08-03_AH02_%03d.tif';
@@ -18,6 +18,11 @@ switch 1
         src_file_name='20150305_AF11_%03d.tif';
         tgt_file_name='20150305_AF11_%03d_resaved.tif';
         nSessions=18;
+    case 3 % 2015-08-02
+        data_folder=fullfile(root_folder,'2015-08-10_AH02');
+        src_file_name='2015-08-10_AH02_%03d.tif';
+        tgt_file_name='resaved/2015-08-10_AH02_%03d.tif';
+        nSessions=26;
 end
 
 
@@ -28,8 +33,8 @@ for session_nr=1:nSessions
         src=Tiff(load_name,'r');
         tag_content=src.getTag('ImageDescription');
         src.close()
-                
-        %new_tag=[tag_content(1:end-1) char(10)];
+                        
+        new_tag=[tag_content(1:end-1) char(10)];
         
         if 1
             %% Write tag to target file
