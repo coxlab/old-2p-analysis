3bbc5bcf11962300c66dd98ca230064457e2dc98
Ben Vermaercke <ben.vermaercke@gmail.com>
2015-08-19 21:50:04 -0400

    Folder selection

--- header_script.m
+++ header_script.m
@@ -58,8 +58,8 @@ switch 0
         %exp_name='2015-08-14_AH03';
         %exp_name='2015-08-14_AH05';
         %exp_name='2015-08-18_AH06';
-        %exp_name='2015-08-19_AH02';
-        exp_name='2015-08-19_AH03';
+        exp_name='2015-08-19_AH02';
+        %exp_name='2015-08-19_AH03';
         
         %exp_name='2015-03-05_AF11/resaved'; % recovered scim bitcodes!
         
