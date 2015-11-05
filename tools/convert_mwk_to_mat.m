0445a1a0d8b7d1240bc5b9914b384e62eeead157
Ben Vermaercke <ben.vermaercke@gmail.com>
2015-11-05 16:01:59 +0100

    Created new tool to convert mwk to mat file
    This will unblock a few issues when using this package on non-mac
    systems

---
+++ tools/convert_mwk_to_mat.m
@@ -0,0 +1,20 @@
+%%% this script will convert .mwk data files to .mat files, everything or parts of it
+%%% only works on mac systems so far.
+%%% perhaps, we could build a python version that is portable across
+%%% systems if python can export to .mat files
+
+%%% in principle, we could crawl the entire data directory and convert
+%%% every .mwk file we encounter. perhaps this will be the default behavior
+%%% if just folder en not file is provided as input.
+
+%%% We are in the process of trying to compile the mwfeval function for
+%%% windows and linux, no luck so far...
+
+clear all
+clc
+
+if ismac
+    
+else
+    disp('Currently, this script only run on mac systems given that the mex file needed to read the .mwk file is only compiled for mac')
+end
