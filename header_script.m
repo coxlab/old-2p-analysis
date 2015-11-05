3c91972a1b277a2995c6d789ea5a12fce51e1608
Ben Vermaercke <ben.vermaercke@gmail.com>
2015-11-05 16:22:40 +0100

    Folder selection

--- header_script.m
+++ header_script.m
@@ -76,10 +76,10 @@ switch 0
             %exp_name='2015-08-19_AH03';
             %exp_name='2015-08-21_AH03'; % funky eyedrift up
             
-            %exp_name='2015-08-14_AH05'; % need ROIs
+            exp_name='2015-08-14_AH05'; % need ROIs
             %exp_name='2015-08-20_AH05'; % need ROIs
             %exp_name='2015-09-01_AH05'; % need ROIs
-            exp_name='2015-09-02_AH05'; % need ROIs
+            %exp_name='2015-09-02_AH05'; % need ROIs
             
             
             %exp_name='2015-08-18_AH06'; % fix bit Codes
