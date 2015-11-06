8eceb4980a74d7da73ee5a92d8879ee95077b756
Ben Vermaercke <ben.vermaercke@gmail.com>
2015-11-06 13:07:48 +0100

    Wrapper needs separate cases for different number of outputs

--- includes/MWorks/get_events_by_name.m
+++ includes/MWorks/get_events_by_name.m
@@ -1,7 +1,21 @@
-function out=get_events_by_name(varargin)
+function varargout=get_events_by_name(varargin)
 
 if ismac
-    out=get_events_by_name_mac(varargin{:});
+    switch nargout
+        case 1
+            varargout{1}=get_events_by_name_mac(varargin{:});
+        case 2
+            [varargout{1},varargout{2}]=get_events_by_name_mac(varargin{:});
+        case 3
+            [varargout{1},varargout{2},varargout{3}]=get_events_by_name_mac(varargin{:});
+    end
 else
-    out=get_events_by_name_xplatform(varargin{:});
+    switch nargout
+        case 1
+            varargout{1}=get_events_by_name_xplatform(varargin{:});
+        case 2
+            [varargout{1},varargout{2}]=get_events_by_name_xplatform(varargin{:});
+        case 3
+            [varargout{1},varargout{2},varargout{3}]=get_events_by_name_xplatform(varargin{:});
+    end
 end
