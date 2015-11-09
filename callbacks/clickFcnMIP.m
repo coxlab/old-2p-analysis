4a3e7fa04d47f8a25a934ad555d6c4caee0f90bf
Ben Vermaercke <ben.vermaercke@gmail.com>
2015-11-09 14:33:21 +0100

    Moved detail calculation to top

--- callbacks/clickFcnMIP.m
+++ callbacks/clickFcnMIP.m
@@ -40,51 +40,54 @@ if isfield(handles,'session_data')
         if between(x,[ext Height-ext])&&between(y,[ext Width-ext])
             %%% Select corresponding part of MIP
             detail=handles.MIP(x-ext+1:x+ext,y-ext+1:y+ext);
-            handles.detail_gamma_val=1;
-            
-            %%% Apply slight blur to get rid of pixelation
-            switch 2
-                case 0
-                    disp('Showing unprocessed detail')
-                case 1
-                    G_size=[3 3];
-                    G=bellCurve2(1,G_size/2+1,[1 1],G_size,0);
-                    detail=convn(detail,G);
-                    detail=detail(G_size(1)-1:end-1,G_size(2)-1:end-1);
-                case 2
-                    FFT_mask=drawCircle2(size(detail,1)/4,[0 0],size(detail));
-                    DC=mean(detail(:));
-                    F=fft2(detail-DC);
-                    detail=real(ifft2(fftshift(fftshift(F).*FFT_mask)))+DC;
-            end
-            switch 2
-                case 1
-                    %%% Save
-                    handles.ROI_selector=handles.nROI+1;
-                    handles.ROI(handles.ROI_selector).base_coord=coord;
-                    handles.ROI(handles.ROI_selector).nCoords=0;
-                    handles.ROI(handles.ROI_selector).coords=[];
-                    handles.detail=detail;
-                case 2
-                    handles.detail=detail;
-                    ROI_temp=handles.ROI_empty;
-                    ROI_temp.base_coord=coord;
-                    handles.ROI_temp=ROI_temp;
-            end
-            
-            %%% Get activity trace for whole window
-            %handles.data_sessions
-            
-            %%% Show
-            set(handles.subplots(1).p(2),'xData',[],'yData',[])
-            set(handles.subplots(2).fig,'cLim',[min(handles.detail(:)) max(handles.detail(:))]);
-            set(handles.subplots(2).h(1),'cData',handles.detail);
-            set(handles.subplots(2).p(1),'xData',[],'yData',[])
-            set(handles.subplots(2).p(2),'xData',[],'yData',[])
         else
             %%% Allow for selections outside of the frame.
-            
+            detail=rand(handles.window_size);
+            %detail=handles.MIP(x-ext+1:x+ext,y-ext+1:y+ext);
         end
+        
+        handles.detail_gamma_val=1;
+        
+        %%% Apply slight blur to get rid of pixelation
+        switch 2
+            case 0
+                disp('Showing unprocessed detail')
+            case 1
+                G_size=[3 3];
+                G=bellCurve2(1,G_size/2+1,[1 1],G_size,0);
+                detail=convn(detail,G);
+                detail=detail(G_size(1)-1:end-1,G_size(2)-1:end-1);
+            case 2
+                FFT_mask=drawCircle2(size(detail,1)/4,[0 0],size(detail));
+                DC=mean(detail(:));
+                F=fft2(detail-DC);
+                detail=real(ifft2(fftshift(fftshift(F).*FFT_mask)))+DC;
+        end
+        switch 2
+            case 1
+                %%% Save
+                handles.ROI_selector=handles.nROI+1;
+                handles.ROI(handles.ROI_selector).base_coord=coord;
+                handles.ROI(handles.ROI_selector).nCoords=0;
+                handles.ROI(handles.ROI_selector).coords=[];
+                handles.detail=detail;
+            case 2
+                handles.detail=detail;
+                ROI_temp=handles.ROI_empty;
+                ROI_temp.base_coord=coord;
+                handles.ROI_temp=ROI_temp;
+        end
+        
+        %%% Get activity trace for whole window
+        %handles.data_sessions
+        
+        %%% Show
+        set(handles.subplots(1).p(2),'xData',[],'yData',[])
+        set(handles.subplots(2).fig,'cLim',[min(handles.detail(:)) max(handles.detail(:))]);
+        set(handles.subplots(2).h(1),'cData',handles.detail);
+        set(handles.subplots(2).p(1),'xData',[],'yData',[])
+        set(handles.subplots(2).p(2),'xData',[],'yData',[])
+        
         guidata(H,handles)
     end    
 end
