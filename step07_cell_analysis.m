db6384d9bb44a205a4dbb69dc0a860108644920d
Ben Vermaercke <ben.vermaercke@gmail.com>
2015-11-12 08:42:14 +0100

    Added calibration and bg image information per rat

--- step07_cell_analysis.m
+++ step07_cell_analysis.m
@@ -9,10 +9,28 @@ dataset_folder=fullfile(dataset_root,animal_ID,'cell_data_files');
 dataset_files=scandir(dataset_folder,'mat');
 nFiles=length(dataset_files);
 
-if ispc
-    im_name='C:\Users\LBP\Documents\GitHub\MotionGUI\Images\2015-08-10_AH03_im.png';
-else
-    im_name='/Users/benvermaercke/CoxLab/MotionGUI/Images/2015-08-10_AH03_im.png';
+offset_correction=[0 0];
+switch animal_ID
+    case 'AH02'
+        calibration_file_name='/Users/benvermaercke/CoxLab/MotionGUI/Calibrations/AH02_20150803.mat';
+        im_name='/Users/benvermaercke/CoxLab/MotionGUI/Images/2015-08-10_AH02_resaved_im.png';
+    case 'AH03'
+        if ispc
+            calibration_file_name='C:\Users\LBP\Documents\GitHub\MotionGUI\Calibrations\AH03_20150807.mat';
+            im_name='C:\Users\LBP\Documents\GitHub\MotionGUI\Images\2015-08-10_AH03_im.png';
+        else
+            calibration_file_name='/Users/benvermaercke/CoxLab/MotionGUI/Calibrations/AH03_20150807.mat';
+            im_name='/Users/benvermaercke/CoxLab/MotionGUI/Images/2015-08-10_AH03_im.png';
+        end
+        %offset_correction=[-.4 .5];
+    case 'AH05'
+        if ispc
+            calibration_file_name='C:\Users\LBP\Documents\GitHub\MotionGUI\Calibrations\AH05_20150901.mat';
+            im_name='C:\Users\LBP\Documents\GitHub\MotionGUI\Images\2015-09-01_AH05_im.png';
+        else
+            calibration_file_name='/Users/benvermaercke/CoxLab/MotionGUI/Calibrations/AH05_20150901.mat';
+            im_name='/Users/benvermaercke/CoxLab/MotionGUI/Images/2015-09-01_AH05_im.png';
+        end
 end
 
 %%
