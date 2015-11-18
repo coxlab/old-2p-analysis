b3c13c863b6eae68ad18e811a00bca4442f5b5bc
Ben Vermaercke <ben.vermaercke@gmail.com>
2015-11-18 14:29:41 +0100

    Perfected rendering of figures

--- step07_cell_analysis.m
+++ step07_cell_analysis.m
@@ -38,7 +38,7 @@ end
 %%
 tic
 cell_data=[];
-for iFile=1:nFiles    
+for iFile=1:nFiles
     load_name=fullfile(dataset_folder,dataset_files(iFile).name);
     S=load(load_name);
     cell_data=cat(2,cell_data,S.cell_data);
@@ -53,29 +53,29 @@ BG=double(imread(im_name))/256;
 BG=flipud(BG);
 figure(333)
 clf
-imshow(BG,[])
+imagesc(BG)
 colormap(green)
 hold on
 marker_size=8;
 p(1)=plot(0,0,'k.','markerSize',marker_size);
 p(2)=plot(0,0,'.','color',[1 1 1]*.4,'markerSize',marker_size);
 p(3)=plot(0,0,'r.','markerSize',marker_size);
-t=title('Variable name');
+t=title(animal_ID);
 axis equal
 axis xy
-axis([400 1000 400 1000])
+axis([570 920 590 920])
 drawnow
-
+colorbar
 
 
 %%
 % cell_locations=zeros(nCells,2);
 % upper_left=zeros(nCells,2);
 % FOV_centers=zeros(nCells,2);
-% 
+%
 % for iCell=1:nCells
-%     [cell_locations(iCell,:), upper_left(iCell,:), FOV_centers(iCell,:)]=cell_data(iCell).get_cell_location();    
-%     
+%     [cell_locations(iCell,:), upper_left(iCell,:), FOV_centers(iCell,:)]=cell_data(iCell).get_cell_location();
+%
 %     %%% re-threshold
 %     cell_data(iCell).do_threshold(2)
 % end
@@ -90,8 +90,8 @@ responsive_positions=cat(1,cell_data.nResponsive_positions);
 %size(cat(1,cell_data.RF_center))
 
 RF_center=cat(1,cell_data.RF_center);
-AZ=RF_center(:,1);
-EL=RF_center(:,2);
+AZ=9-RF_center(:,1); % flipped azimuth direction: like this 1=center 8=periphery
+EL=5-RF_center(:,2); % flipped, 1=bottom 4=top
 RF_sizes=cat(1,cell_data.RF_size);
 cell_size=cat(1,cell_data.cell_size);
 
@@ -106,8 +106,8 @@ end
 invariance_avg=cat(1,cell_data.invariance_avg);
 %%
 
-analysis_variable_names={'Responsive','Selective positions','Azimuth','Elevation','RF size','Cell Size','Sparseness','Invariance'};
-analysis_variable=3;
+analysis_variable_names={'Responsive','Selective positions','Azimuth','Elevation','RF size','Cell Size','Sparseness','Invariance','Angle'};
+analysis_variable=4;
 
 %sel1=false(nCells,1);
 sel1=responsive_positions>0;
@@ -116,7 +116,7 @@ switch analysis_variable
     case 1
         sel2=responsive_positions>0;
         %sel2=responsive_positions>0;
-    case 2        
+    case 2
         sel2=responsive_positions>4;
         %figure(334)
         %MAP_avg=mean(cat(3,cell_data(sel2).RF_map_TH),3);
@@ -124,7 +124,7 @@ switch analysis_variable
         %xlabel('Periphery <=> Center')
     case 3
         sel2=sel1==1&AZ<=4; % periphery to center 1-8
-        %sel2=sel1==1&AZ>=9 - 5; % center to periphery 
+        %sel2=sel1==1&AZ>=9 - 5; % center to periphery
     case 4
         sel2=sel1==1&EL<=2;
     case 5 % Receptive field size
@@ -138,42 +138,30 @@ switch analysis_variable
         
     case 9
         %V1_coords=[5899 8957 ; 5888 6444 ; 7521 8957];
-        V1_coords=[5790 6160 ; 5790 9140 ; 7770 9140];
-        sel2=inpolygon(cell_locations(:,1),cell_locations(:,2),V1_coords(:,1),V1_coords(:,2));
-        
+        %V1_coords=[5790 6160 ; 5790 9140 ; 7770 9140];
+        %sel2=inpolygon(cell_locations(:,1),cell_locations(:,2),V1_coords(:,1),V1_coords(:,2));
+        %case 9
         % combos
-    case 10 
+    case 10
         %sel1=responsive_cells>0&cell_size>20*10;
         sel2=sel1==1&AZ<=1;
-    case 11 
+    case 11
         %sel1=responsive_cells>0&cell_size>12*10;
         sel2=sel1==1&AZ>=5;
     case 12
         %sel1=responsive_cells>0&cell_size>12*10;
-        sel2=sel1==1&EL>=4;        
+        sel2=sel1==1&EL>=4;
 end
 
 [length(sel1) sum(sel1) sum(sel2)]
 tabulate(sel1)
 tabulate(sel2(sel1))
 
-%%%
-%figure(333)
-%plot(cell_data(1).offset(1)*resize_factor,cell_data(1).offset(2)*resize_factor,'rs')
-
-%plot(FOV_centers(:,1)*resize_factor,FOV_centers(:,2)*resize_factor,'yo')
-%plot(upper_left(:,1)*resize_factor,upper_left(:,2)*resize_factor,'rs')
-%plot(cell_locations(:,1)*resize_factor,cell_locations(:,2)*resize_factor,'ks')
-
-%plot(cell_locations(sel1,1)*resize_factor,cell_locations(sel1,2)*resize_factor,'k.')
-%plot(cell_locations(sel2,1)*resize_factor,cell_locations(sel2,2)*resize_factor,'r.')
-
-
+%%
 set(p(2),'xData',cell_locations(~sel1,1)*resize_factor,'yData',cell_locations(~sel1,2)*resize_factor)
 set(p(1),'xData',cell_locations(sel1,1)*resize_factor,'yData',cell_locations(sel1,2)*resize_factor)
 set(p(3),'xData',cell_locations(sel2,1)*resize_factor,'yData',cell_locations(sel2,2)*resize_factor)
 set(t,'string',analysis_variable_names{analysis_variable})
-axis([570 920 590 920])
 %hold off
 
 %% plot the percentage of neuron within a certain area, relative to scambled values
@@ -201,7 +189,7 @@ switch which_space
         
         F=full(S);
         F_s=imresize(F,.2);
-                        
+        
         imshow(rot90(F_s),[])
         colormap jet
     case 2
@@ -213,11 +201,18 @@ switch which_space
         window_size=250; % window size in which to look for cells if selection_region is rect
         inclusion_radius=125; % if selection_region is circle
         nCells_min=3; % minimal number of cells before doing analysis
-                
-        X=cell_locations(:,1);
-        Y=cell_locations(:,2);
-        range_x=[min(X) max(X)];
-        range_y=[min(Y) max(Y)];
+        
+        switch 2
+            case 1
+                X=cell_locations(:,1);
+                Y=cell_locations(:,2);
+                range_x=[min(X) max(X)];
+                range_y=[min(Y) max(Y)];
+            case 2
+                axis([570 920 590 920])
+                range_x=[570 920]*10;
+                range_y=[590 920]*10;
+        end
         nSteps_x=round(diff(range_x)/stride_length);
         nSteps_y=round(diff(range_y)/stride_length);
         [rows,cols]=meshgrid(linspace(range_x(1),range_x(2),nSteps_x),linspace(range_y(1),range_y(2),nSteps_y));
@@ -225,7 +220,7 @@ switch which_space
         R=round(rows(:));
         C=round(cols(:));
         
-        N=length(R);        
+        N=length(R);
         res_image_vector=zeros(N,1);
         res_image_vector_std=res_image_vector;
         pos_rect=[0 0 window_size window_size];
@@ -246,40 +241,115 @@ switch which_space
                 case 'circle'
                     [theta,rho]=cart2pol(X-R(iPos),Y-C(iPos));
                     sel=rho<inclusion_radius&pre_selection;
-            end            
-            if sum(sel)>nCells_min                
+            end
+            if sum(sel)>nCells_min
                 switch analysis_variable
-                    case 1 % %responsive                        
+                    case 1 % %responsive
                         res_image_vector(iPos)=mean(responsive_positions(sel)>0)*100;
                     case 2 % #responsive positions
-                        res_image_vector(iPos)=mean(responsive_positions(sel));                        
+                        res_image_vector(iPos)=mean(responsive_positions(sel));
                     case 3 % Azimuth
-                        res_image_vector(iPos)=mean(9-AZ(sel));
-                        res_image_vector_std(iPos)=std(9-AZ(sel));
+                        res_image_vector(iPos)=mean(AZ(sel));
+                        res_image_vector_std(iPos)=std(AZ(sel));
+                        if 0
+                            %% render colorscale
+                            figure(457)
+                            I=repmat(9-linspace(1,8,256),128,1);
+                            imshow(I,[])
+                            %imagesc(I)
+                            colormap(jet)
+                            axis xy
+                            xlabel('periphery to center')
+                            ylabel('bottom to top')
+                        end
                     case 4 % elevation
-                        res_image_vector(iPos)=mean(4-EL(sel));
+                        res_image_vector(iPos)=mean(EL(sel));
+                        res_image_vector_std(iPos)=std(EL(sel));
+                        
+                        if 0
+                            %% render colorscale
+                            figure(457)
+                            %I=repmat(9-linspace(1,8,256),128,1);
+                            I=repmat(linspace(1,4,128),256,1)';
+                            imshow(I,[])
+                            %imagesc(I)
+                            colormap(jet)
+                            axis xy
+                            xlabel('periphery to center')
+                            ylabel('bottom to top')
+                        end
                     case 5 % receptive field size
                         res_image_vector(iPos)=mean(RF_sizes(sel));
-                    case 6 % sparseness                         
+                    case 7 % sparseness
                         res_image_vector(iPos)=mean(sparseness_max(sel))*100;
-                    case 7 % invariance
-                        res_image_vector(iPos)=mean(invariance_avg(sel))*100;                        
+                    case 8 % invariance
+                        res_image_vector(iPos)=mean(invariance_avg(sel))*100;
+                        
+                    case 9 % combo of AZ and EL => angle relative to screen center
+                        [theta,rho]=cart2pol(AZ(sel)-mean([1 8]),EL(sel)-mean([1 4]));
+                        
+                        %N_test=1000;
+                        %X_test=rand(N_test,1)*8-4;
+                        %Y_test=rand(N_test,1)*4-2;
+                        
+                        %X_test=AZ(pre_selection)-4.5;
+                        %Y_test=EL(pre_selection)-2.5;
+                        %plot(0,0,'m*')
+                        %hold on
+                        %plot(X_test,Y_test,'.')
+                        %hold off
+                        %[theta,rho]=cart2pol(X_test,Y_test);
+                        
+                        % zero angle is peripheral center of screen
+                        %[theta,rho]=cart2pol(1-mean([1 8]),2.5-mean([1 4]));
+                        
+                        theta=theta/pi*180+180;
+                        res_image_vector(iPos)=mean(theta);
+                        res_image_vector_std(iPos)=std(theta);
+                        
+                        if 0
+                            %% render colorscale
+                            figure(457)
+                            [a,b]=meshgrid(linspace(1,8,256),linspace(1,4,128));
+                            [theta,rho]=cart2pol(9-a(:)-mean([1 8]),b(:)-mean([1 4]));
+                            I=reshape(theta,128,256);
+                            imshow(I,[])
+                            %imagesc(I)
+                            colormap(jet)
+                            axis xy
+                            xlabel('periphery to center')
+                            ylabel('bottom to top')
+                        end
                 end
             end
-        end    
+        end
         toc
         
         %% Show map
         res_image=reshape(res_image_vector,nSteps_y,nSteps_x);
         res_image_smooth=imgaussfilt(res_image,3);
         
+        if any(res_image_vector_std>0)
+            res_image_std=reshape(res_image_vector_std,nSteps_y,nSteps_x);
+            res_image_std_smooth=imgaussfilt(res_image_std,1);
+        end
         
-        res_image_std=reshape(res_image_vector_std,nSteps_y,nSteps_x);
-        res_image_std_smooth=imgaussfilt(res_image_std,3);
+        figure(456)
+        clf
+        add_BG=0;
+        if add_BG==1
+            BG_cutout=BG(range_y(1)/10+1:range_y(2)/10,range_x(1)/10+1:range_x(2)/10,2);
+            BG_cutout=BG_cutout/max(BG_cutout(:))*max(res_image_smooth(:));
+            res_image_smooth(res_image(:)==0)=BG_cutout(res_image(:)==0);
+        end
         
-        figure(457)
         H=imagesc((res_image_smooth));
-        %set(H,'AlphaData',res_image_std_smooth)
+        %H=imshow(res_image_smooth,[]);
+        if any(res_image_vector_std>0)&&add_BG==0
+            set(H,'AlphaData',res_image_std)
+            %set(H,'AlphaData',res_image_std_smooth)
+        end
+        
         axis xy
         axis square
         set(gca,'XTickLabel',get(gca,'XTick')*stride_length)
@@ -288,8 +358,12 @@ switch which_space
         ylabel('Posterior-Anterior position (µm)')
         title(sprintf([analysis_variable_names{analysis_variable} ' (%d out of %d neurons)'],[nSelected_neurons nCells]))
         set(gca,'CLim',[min(res_image(res_image(:)>0)) max(res_image(res_image(:)>0))])
-        colormap(jet)
-        colorbar        
+        if ismember(analysis_variable,9)
+            colormap(jet)
+        else
+            colormap(jet)
+        end
+        colorbar
         
         if 0
             %%
@@ -312,7 +386,7 @@ V1_selection=inpolygon(cell_locations(:,1),cell_locations(:,2),V1_coords(:,1),V1
 
 cell_selection=responsive_positions>1&cell_size>0;
 
-%% Receptive field sizes 
+%% Receptive field sizes
 % M=[responsive_positions V1_selection RF_sizes];
 % M=M(responsive_positions>0,:);
 
@@ -337,7 +411,7 @@ fprintf('RF_sizes (V1:%3.2f vs. LM:%3.2f) p=%3.2f (N=%d) \n',[m_V1 m_LM p_value 
 % sel1=responsive_positions>0&V1_selection==1;
 % sel2=sel1==1&sparseness_avg>3/10;
 % tabulate(sel2(sel1))
-% 
+%
 % % extrastriate
 % sel1=responsive_positions>0&V1_selection==0;
 % sel2=sel1==1&sparseness_avg>3/10;
@@ -396,7 +470,7 @@ fprintf('Invariance (V1:%3.2f vs. LM:%3.2f) p=%3.2f (N=%d) \n',[m_V1 m_LM p_valu
 
 
 
-%%% Story: 
+%%% Story:
 % for cells with at least 2 RF positions
 % median RF size is slightly bigger (ns. p=0.51)
 % median sparseness at best position is lower (p=0.02, ns. if averaged over positions p=0.39)
